30bfba42017c4a257a798d06b75ffe48e87c3a30
macbeth <layne.wegenast@gmail.com>
2022-02-15 14:59:22 -0600

    for added to javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,31 @@
+// var playerName = 'bobo';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-//You can also log multiple values at once like this console.log (playerName, playerAttack, playerHealth);
+//You can also log multiple values at once like this 
+console.log (playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames [0]);
+console.log(enemyNames [1]);
+console.log(enemyNames [2]);
+console.log(enemyNames.length)
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var playerMoney = 10;
-
+//fight function
 var fight = function() {
     //alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
+    // ask player if they'd like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if player choses to fight, then fight
@@ -58,7 +70,11 @@ var fight = function() {
     else {
       fight();
     }
+    // if player did not chose 1 or 2 in prompt
+  } else {
+    window.alert("You need to pick a valid option. Try again!");
   }
 }
  
-fight();
+// run fight function to start game
+// fight();
